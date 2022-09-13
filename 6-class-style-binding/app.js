0c8afeeefcd2bf7e2d2d9fb8962e69c5c7f842bd
sebahattin <sebkucuk05@gmail.com>
2022-09-13 19:40:04 +0300

    class-style-binding

--- 6-class-style-binding/app.js
+++ 6-class-style-binding/app.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      showBorder: false,
+      redBG: false,
+      boxClass: "border green",
+      bgColor: "cyan",
+      styleBorder: "4px dashed rgb(202, 17, 178)",
+      styleTextcolor: "blue",
+    };
   },
-});
-app.mount("#app");
+  computed: {
+    boxClasses() {
+      return { border: this.showBorder, red: this.redBG };
+      // eğer burada tireli - birşey kullanacaksan tırnaklar içerisinde  kullanabilirsin. Javascript kuralı geçerli. border yerine "border-aa" bu şekilde kullanabilirsin.
+    },
+  },
+}).mount("#app");
 
 // setTimeout(() => {
 //   app.unmount();
